4bd6cd501858696d3404f8f204706e29753a3977
felipe gomes <felipinhogoma@gmail.com>
2020-10-09 04:51:24 -0300

    53bea4a48087eed68d9f177bed5dbf685dcc3dcf

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -12,16 +12,19 @@ export class AppComponent {
   aluno: Aluno = { nome: "Felipe", cpf: "12345", email: "f@cin.ufpe.br", github: "fgsantana" };
   alunoService = new AlunoService();
   alunos: Aluno[] = [];
+  cpfduplicado: boolean = false;
 
   gravar(a: Aluno): void {
     if (this.alunoService.gravar(a)) {
       this.alunos.push(a);
       this.aluno = {nome: "", cpf: "", email: "",github: " " };
     } else {
-      this.aluno.cpf = "";
-      alert("Já existe um aluno com esse CPF");
+      this.cpfduplicado = true;
     }
  }
+ onMove(): void {
+  this.cpfduplicado = false;
+}
 }
 
 
