87bc4c4ab045ac3c543a5a10549d42799a267132
felipe gomes <felipinhogoma@gmail.com>
2020-10-09 01:06:46 -0300

    Conteúdo do commit dbc075633eeea5de33883e470e0c203c66b8d566

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,4 +1,6 @@
+  
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ta-gui';
+   aluno: Aluno = {nome: "", cpf: "", email: ""};
+}
+
+export class Aluno {
+  nome: string;
+  cpf: string;
+  email: string;
 }
