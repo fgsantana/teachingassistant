e40456c74059f19aaffd6ed0077ec91b058d20e4
felipe gomes <felipinhogoma@gmail.com>
2020-10-09 04:41:57 -0300

    Conteúdo do commit 5b0dfe23f87640e0fc02a6f55072c2211a0f1db8

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent {
       this.aluno = {nome: "", cpf: "", email: "",github: " " };
     } else {
       this.aluno.cpf = "";
+      alert("Já existe um aluno com esse CPF");
     }
  }
 }
